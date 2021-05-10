2fe775255481e24aea113404e935f7898e971c3b
Ishan Lal <ishanlal@gmail.com>
2021-05-10 09:55:53 -0700

    renaming components

--- components/CardView.js
+++ components/CardView.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class AnswerView extends React.Component {
+export default class CardView extends React.Component {
 
   render() {
 
     return (
       <View>
-        <Text>Answer View</Text>
+        <Text>Card View</Text>
       </View>
     )
   }
